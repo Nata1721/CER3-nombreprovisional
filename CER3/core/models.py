41a1118dffaad1149f426b185196c31e494c7f3f
Nata <natanael.roque@usm.cl>
2023-11-21 20:38:48 -0300

    .models modificado

--- CER3/core/models.py
+++ CER3/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    TIPO = [
+        ("Vacaciones"),
+        ("Feriado"),
+        ("Suspensión de actividades"),
+        ("Suspensión de actividades PM"),
+        ("Periodo Lectivo"),
+        ("Suspensión de evaluaciones"),
+        ("Ceremonia"),
+        ("EDDA"),
+        ("Evaluación"),
+        ("Ayudantías"),
+        ("Hito Académico"),
+        ("Secretaría Académica"),
+        ("OAI"),
+    ]
+
+    SEGMENTO = [
+        ("Comunidad USM"),
+        ("Estudiante"),
+        ("Profesor"),
+        ("Jefe de Carrera"),
+    ]
+
+    fechaInicio = models.DateTimeField(auto_now_add=False)
+    fechaTermino = models.DateTimeField(auto_now_add=False)
+    titulo = models.CharField(max_length=55)
+    descripcion = models.CharField(max_length=100)
+    tipo = TIPO
+    segmento = SEGMENTO
