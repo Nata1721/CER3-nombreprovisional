25cb91a093878ef951091773b2b579213c89a258
Nata <natanael.roque@usm.cl>
2023-11-21 20:49:06 -0300

    admin

--- CER3/core/admin.py
+++ CER3/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Evento
 # Register your models here.
+
+class datos_evento(admin.ModelAdmin):
+    list_display = ("titulo", "descripcion", "tipo", "segmento", "fechaInicio", "fechaTermino")
+    list_filter = ("fechaInicio", "fechaTermino", )
+
+admin.site.register(Evento, datos_evento)
+
